a914871e5cca5bdffb74fa265882abe5753b8d61
Peterson Umoke <umoke10@hotmail.com>
2021-05-12 00:23:17 +0100

    adding various compilations of th eapp

--- webpack_admin.mix.js
+++ webpack_admin.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
 
+require('laravel-mix-merge-manifest');
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -19,7 +21,8 @@ mix
         require('tailwindcss'),
         require('autoprefixer'),
     ])
-    .webpackConfig(require('./webpack.config'));
+    .webpackConfig(require('./webpack.config'))
+    .mergeManifest();
 
 if (mix.inProduction()) {
     mix.version();
