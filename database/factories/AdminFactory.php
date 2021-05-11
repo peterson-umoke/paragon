4c4d591bfa503f04d12014e467dbda6ac641d676
Peterson Umoke <umoke10@hotmail.com>
2021-05-12 00:56:56 +0100

    setup the admin factory class

---
+++ database/factories/AdminFactory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Admin;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AdminFactory extends Factory
+{
+    protected $model = Admin::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => $this->faker->password,
+        ];
+    }
+
+    /**
+     * @param $email
+     * @return AdminFactory
+     */
+    public function email($email): AdminFactory
+    {
+        return $this->state(fn(array $attrs) => ['email' => $email]);
+    }
+
+    /**
+     * @param $email
+     * @return AdminFactory
+     */
+    public function password($password): AdminFactory
+    {
+        return $this->state(fn(array $attrs) => ['password' => $password]);
+    }
+
+    /**
+     * @param $email
+     * @return AdminFactory
+     */
+    public function name($name): AdminFactory
+    {
+        return $this->state(fn(array $attrs) => ['name' => $name]);
+    }
+}
