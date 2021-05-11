38e8d68240fec3ec5043a46dd759639c99b6413a
Peterson Umoke <umoke10@hotmail.com>
2021-05-12 00:48:33 +0100

    done with the auth for user

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -9,17 +9,7 @@ use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
-    public function index()
-    {
-        return inertia()->render('Welcome', [
-            'canLogin' => Route::has('login'),
-            'canRegister' => Route::has('register'),
-            'laravelVersion' => Application::VERSION,
-            'phpVersion' => PHP_VERSION,
-        ]);
-    }
-
-    public function show()
+    public function __invoke()
     {
         return Inertia::render('Dashboard');
     }
