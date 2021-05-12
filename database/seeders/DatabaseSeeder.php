f43e4f4ea5696e37a3ef449287cc0fee6f8f01f9
Peterson Umoke <umoke10@hotmail.com>
2021-05-12 01:02:45 +0100

    fixed bug where admin was not created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        Admin::factory()->email('super@app.com')->password('password')->create(1);
-        Admin::factory()->create(9);
+        Admin::factory()->email('super@app.com')->password('password')->create();
+        Admin::factory()->count(9)->create();
     }
 }
